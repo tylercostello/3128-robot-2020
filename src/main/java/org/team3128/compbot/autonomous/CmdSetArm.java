bd05d42fd338e5ee892e360f537c3a3895ce8b59
MikaO13 <mika.disneypins@gmail.com>
2020-02-19 15:39:32 -0800

    just making it so I can pull

---
+++ src/main/java/org/team3128/compbot/autonomous/CmdSetArm.java
@@ -0,0 +1,16 @@
+package org.team3128.compbot.autonomous;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class CmdSetArm extends Command {
+    public CmdSetArm() {
+        
+    }
+
+    @Override
+    protected boolean isFinished() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
