147ca62bb24ecf5e2dc4539b0ef88c2611280091
Adham Elarabawy <adhamelarabawy@gmail.com>
2020-02-25 10:59:42 -0800

    Constants cleaning

--- src/main/java/org/team3128/compbot/subsystems/Constants.java
+++ src/main/java/org/team3128/compbot/subsystems/Constants.java
@@ -18,7 +18,6 @@ public class Constants extends RobotConstants {
         public static class GameConstants {
                 public static final double SHOOTER_TARGET_HEIGHT = 98.25; // height of target in INCHES
                 public static final double BALL_HEIGHT = 9; // height of the ball in centimeters
-                                                            // TODO: check ball height
         }
 
         public static class MechanismConstants {
@@ -31,12 +30,7 @@ public class Constants extends RobotConstants {
                 public static final double kDriveInchesPerSecPerNUp100ms = (1000d / 1)
                                 * (1 / MechanismConstants.ENCODER_RESOLUTION_PER_ROTATION)
                                 * (Constants.DriveConstants.WHEEL_DIAMETER * Math.PI)
-                                * Constants.DriveConstants.WHEEL_ROTATIONS_FOR_ONE_ENCODER_ROTATION;
-                // a fairly basic relationship between tangential and
-                // rotational speed: NU/100ms * 1000ms/1second * 1/(encoder resolution) *
-                // CIRCUM *
-                // (relation between encoder rotations and wheel
-                // rotations) = in/s
+                                * Constants.DriveConstants.WHEEL_ROTATIONS_FOR_ONE_ENCODER_ROTATION; // a fairly basic relationship between tangential and rotational speed: NU/100ms * 1000ms/1second * 1/(encoder resolution) * CIRCUM * (relation between encoder rotations and wheel rotations) = in/s
                 public static final double kDriveNuToInches = (1
                                 / Constants.MechanismConstants.ENCODER_RESOLUTION_PER_ROTATION)
                                 * Constants.DriveConstants.WHEEL_DIAMETER * Math.PI
@@ -44,74 +38,33 @@ public class Constants extends RobotConstants {
 
                 public static final NeutralMode DRIVE_IDLE_MODE = NeutralMode.Brake;
 
-                public static final double ENCODER_ROTATIONS_FOR_ONE_WHEEL_ROTATION = 72 / 8; // basically your gearing.
-                                                                                              // Ask
-                                                                                              // Mech
-                                                                                              // for gear teeth number
-                                                                                              // to gear
-                                                                                              // teeth number ratio:
-                                                                                              // 8.3333333
+                public static final double ENCODER_ROTATIONS_FOR_ONE_WHEEL_ROTATION = 72 / 8; // basically your gearing. Ask Mech for gear teeth number to gear teeth number ratio: 8.3333333
 
                 public static final double WHEEL_ROTATIONS_FOR_ONE_ENCODER_ROTATION = 1
                                 / Constants.DriveConstants.ENCODER_ROTATIONS_FOR_ONE_WHEEL_ROTATION;
 
                 public static final int RIGHT_DRIVE_FRONT_ID = 3;
                 public static final int RIGHT_DRIVE_MIDDLE_ID = 2;
-                // public static final int RIGHT_DRIVE_BACK_ID = 0;
 
                 public static final int LEFT_DRIVE_FRONT_ID = 1;
                 public static final int LEFT_DRIVE_MIDDLE_ID = 0;
-                // public static final int LEFT_DRIVE_BACK_ID = 3;
 
                 public static final int DRIVE_HIGH_SPEED = 140; // Empirical Max Linear Speed: TBD in/s
 
-                public static final double WHEEL_DIAMETER = 3.55; // effective wheel diameter (measure first then tune
-                                                                  // this
-                                                                  // number until distances are accurate)
+                public static final double WHEEL_DIAMETER = 3.55; // effective wheel diameter (measure first then tune this number until distances are accurate)
 
-                public static final double LEFT_SPEEDSCALAR = 1.0; // purely for TELEOP drive (to make sure that when
-                                                                   // the drive
-                                                                   // pushes the joystick forward, both sides of the
-                                                                   // drivetrain
-                                                                   // are
-                                                                   // going ROUGHLY the same speed)
-                public static final double RIGHT_SPEEDSCALAR = 1.0;// purely for TELEOP drive (to make sure that when
-                                                                   // the drive
-                                                                   // pushes the joystick forward, both sides of the
-                                                                   // drivetrain
-                                                                   // are
-                                                                   // going ROUGHLY the same speed)
+                public static final double LEFT_SPEEDSCALAR = 1.0; // purely for TELEOP drive (to make sure that when the drive pushes the joystick forward, both sides of the drivetrain are going ROUGHLY the same speed)
+                public static final double RIGHT_SPEEDSCALAR = 1.0;// purely for TELEOP drive (to make sure that when the drive pushes the joystick forward, both sides of the drivetrain are going ROUGHLY the same speed)
 
-                public static final double DRIVE_ACCEL_LIMIT = 120; // Ballpark estimates from mech (Be conservative
-                                                                    // unless you
-                                                                    // really need the quick auto paths)
+                public static final double DRIVE_ACCEL_LIMIT = 120; // Ballpark estimates from mech (Be conservative unless you really need the quick auto paths)
                 public static final double DRIVE_JERK_LIMIT = 2000; // Ballpark estimates (Be conservative)
 
                 public static double K_AUTO_RIGHT_P = 0.00007; // 0.00065
                 public static double K_AUTO_RIGHT_D = 0.000;
-                public static double K_AUTO_RIGHT_F = 1 / 145.9150145782 * kDriveInchesPerSecPerNUp100ms; // 1/
-                                                                                                          // (consistent
-                                                                                                          // max
-                                                                                                          // vel
-                                                                                                          // of this
-                                                                                                          // side of
-                                                                                                          // drivetrain
-                                                                                                          // in/s) *
-                                                                                                          // conversion
-                                                                                                          // to NU/s
+                public static double K_AUTO_RIGHT_F = 1 / 145.9150145782 * kDriveInchesPerSecPerNUp100ms; // 1/(consistent max vel of this side of drivetrain in/s) * conversion to NU/s
                 public static double K_AUTO_LEFT_P = 0.00007;
                 public static double K_AUTO_LEFT_D = 0.000; // 0.0001
-                public static double K_AUTO_LEFT_F = 1 / 140.8705712261 * kDriveInchesPerSecPerNUp100ms; // 1/
-                                                                                                         // (consistent
-                                                                                                         // max
-                                                                                                         // vel
-                                                                                                         // of
-                                                                                                         // this side of
-                                                                                                         // drivetrain
-                                                                                                         // in/s) *
-                                                                                                         // conversion
-                                                                                                         // to
-                                                                                                         // NU/s
+                public static double K_AUTO_LEFT_F = 1 / 140.8705712261 * kDriveInchesPerSecPerNUp100ms; // 1/(consistent max vel of this side of drivetrain in/s) * conversion to NU/s
                 public static final double K_HOLD_P = 4;
         }
 
@@ -131,8 +84,7 @@ public class Constants extends RobotConstants {
                 public static final double BOTTOM_LIMELIGHT_ANGLE = 26.0 * Angle.DEGREES;
                 public static final double BOTTOM_LIMELIGHT_DISTANCE_FROM_FRONT = 1 * Length.in;
                 public static final int SAMPLE_RATE = 3;
-                public static final double TX_THRESHOLD = 1; // the maximum error in tx where the shooter will be
-                                                             // allowed to shoot
+                public static final double TX_THRESHOLD = 1; // the maximum error in tx where the shooter will be allowed to shoot
                 public static final double TX_OFFSET = 0; // to offset alignment in either direction
                 public static final PIDConstants VISION_PID = new PIDConstants(0, 0.014, 0.0, 0.00003);
                 public static final PIDConstants BALL_PID = new PIDConstants(0.57, 0.02, 0.0, 0.00003);
@@ -161,19 +113,11 @@ public class Constants extends RobotConstants {
                 public static final int SHOOTER_MOTOR_LEFT_ID = 8;
                 public static final int SHOOTER_MOTOR_RIGHT_ID = 7;
 
-                public static final double SHOOTER_GEARING = 1.5; // for every 1 rotation of the motor, the shooter does
-                                                                  // {SHOOTER_GEARING} rotations
+                public static final double SHOOTER_GEARING = 1.5; // for every 1 rotation of the motor, the shooter does {SHOOTER_GEARING} rotations
                 public static final PIDConstants SHOOTER_PID = new PIDConstants(0, 0.005, 0.009, 0.0000051);
 
-                public static final double SHOOTER_SATURATION_LIMIT = 5; // set limit on integral accumulation (in
-                                                                         // this
-
-                                                                         // case, 1
-                                                                         // volt)
-                public static final double RPM_THRESHOLD = 100; // the maximum difference between an RPM and the
-                                                                // setpoint
-                                                                // for a
-                                                                // data point to be considered as a plataeu
+                public static final double SHOOTER_SATURATION_LIMIT = 5; // set limit on integral accumulation (in this case, 1 volt)
+                public static final double RPM_THRESHOLD = 100; // the maximum difference between an RPM and the setpoint for a data point to be considered as a plataeu
                 public static final int PLATEAU_COUNT = 25; // 50 * 0.005s = 0.25 seconds of plateau required
         }
 
@@ -192,7 +136,6 @@ public class Constants extends RobotConstants {
                 public static final int CAPACITY = 5; // num of balls that the hopper can store
                 public static final double BASE_POWER = -0.3;
                 public static final double FEEDER_REVERSE = 0.25;
-                // public static final double FEEDER_POWER = 0.5;
                 public static final double GATEKEEPER_POWER = -0.75;
                 public static final double INDEXER_POWER = -0.4;
                 public static final double[] BALL_SPACING = { -25, -20, -20, -20 }; //offsets for the spacing out the balls in the hopper
@@ -205,7 +148,7 @@ public class Constants extends RobotConstants {
 
                 public static final double INTAKE_MOTOR_ON_VALUE = 0.7;
                 public static final double INTAKE_MOTOR_OFF_VALUE = 0;
-                public static final double INTAKE_MOTOR_REVERSE_VALUE = -0.5;
+                public static final double INTAKE_MOTOR_REVERSE_VALUE = -0.5; //for clearing jams
         }
         
         public static class ClimberConstants {
