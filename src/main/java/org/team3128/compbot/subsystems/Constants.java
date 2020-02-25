00682a01e881fb03d63048b755ba3dc026fa1c40
Adham Elarabawy <adhamelarabawy@gmail.com>
2020-02-25 08:45:52 -0800

    Fixed Constants Error

--- src/main/java/org/team3128/compbot/subsystems/Constants.java
+++ src/main/java/org/team3128/compbot/subsystems/Constants.java
@@ -206,8 +206,8 @@ public class Constants extends RobotConstants {
                 public static final double INTAKE_MOTOR_ON_VALUE = 0.7;
                 public static final double INTAKE_MOTOR_OFF_VALUE = 0;
                 public static final double INTAKE_MOTOR_REVERSE_VALUE = -0.5;
+        }
         
-
         public static class ClimberConstants {
                 public static final int LEFT_SERVO_ID = 0;
                 public static final int RIGHT_SERVO_ID = 1;
