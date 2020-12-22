41f1909d257066a46ce1a2ae4fe06454afffe1cc
Team 3128: Controls <controls@team3128.org>
2020-12-21 16:09:39 -0800

    EKF theta lower variance

--- src/main/java/org/team3128/compbot/main/MainCompbot.java
+++ src/main/java/org/team3128/compbot/main/MainCompbot.java
@@ -90,7 +90,7 @@ public class MainCompbot extends NarwhalRobot {
     //Navx should be pretty accurate because it is running its own kalman filter
     //I have no idea what the encoder variances should be
     static EKF ekf = new EKF(0, 0, Math.PI/2, 0, 0, 10, 10, 0.5842,//0.9652,
-    0.01, 0.1, 0.01, 0.01);
+    0.01, 1e-3, 0.01, 0.01);
     private double[] inputArray = new double[4];
     private double[] outputArray;
     private double currentTime, previousTime, printerTime, initTime;
